c2205b96561ab391ad64c9ca63366f8216eb9edc
OwenScott-GDS <92757524+owenscott-gds@users.noreply.github.com>
2021-12-02 11:48:25 -0800

    created a Data instance with the plantName passed to it Generated a OneTimeWorkRequest with the passed in duration, time unit, and data  instance
     Enqueued the request as a unique work request

--- app/src/main/java/com/example/waterme/viewmodel/PlantViewModel.kt
+++ app/src/main/java/com/example/waterme/viewmodel/PlantViewModel.kt
@@ -16,27 +16,49 @@
 package com.example.waterme.viewmodel
 
 import android.app.Application
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.work.Data
+import androidx.work.ExistingWorkPolicy
+import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
 import com.example.waterme.data.DataSource
+import com.example.waterme.worker.WaterReminderWorker
 import java.util.concurrent.TimeUnit
 
 class PlantViewModel(application: Application): ViewModel() {
 
+    private val workManager = WorkManager.getInstance(application)
+
     val plants = DataSource.plants
 
+    @RequiresApi(Build.VERSION_CODES.O)
     internal fun scheduleReminder(
         duration: Long,
         unit: TimeUnit,
         plantName: String
     ) {
+
         // TODO: create a Data instance with the plantName passed to it
+        val data =   Data.Builder().putString(WaterReminderWorker.nameKey, plantName).build()
+
+
 
         // TODO: Generate a OneTimeWorkRequest with the passed in duration, time unit, and data
         //  instance
+        val request = OneTimeWorkRequest.Builder(WaterReminderWorker::class.java)
+            .setInitialDelay(duration,unit)
+            .setInputData(data)
+            .build()
+
 
         // TODO: Enqueue the request as a unique work request
+
+        workManager.enqueueUniqueWork(plantName, ExistingWorkPolicy.REPLACE, request)
+
+
     }
 }
 
